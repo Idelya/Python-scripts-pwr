345e2c85cc4d49dd604a7273536dcfe52759b463
Anna <annapapros21@gmail.com>
2020-10-20 00:27:02 +0200

    almost finished

--- main.py
+++ main.py
@@ -1,69 +1,118 @@
 from typing import List, Dict, Callable, Union
 from students import students_arr
 
+students_list = students_arr
+student_list_helper = []
+
+
+def print_stud_short_with_key(stud: Dict[str, Union[str, int, float]], key: str):
+    print("STUDENT: ", stud.get('imie'), ' ', stud.get('nazwisko'), ' - ', stud.get(key))
+
+
+def print_stud(stud: Dict[str, Union[str, int, float]]):
+    print("STUDENT")
+    for key in stud:
+        print(key, ': ', stud.get(key))
+
 
 def add_student():
-    pass
+    new_stud = {'imie': '',
+                'nazwisko': '',
+                'PESEL': '',
+                'plec': '',
+                'rok_studiow': 0,
+                'srednia_ocen': 0.0,
+                'kierunek_studiow': ''}
+
+    for key in new_stud:
+        value = input(key + ':')
+        if key=='rok_studiow' :
+            new_stud[key] = int(value)
+        elif key=='srednia_ocen' :
+            new_stud[key] = float(value)
+        else:
+            new_stud[key] = value
+
+    print("Added new student:")
+    print_stud(new_stud)
+    students_list.append(new_stud)
 
 
 def print_all():
-    pass
+    for stud in students_list:
+        print_stud_short_with_key(stud, 'PESEL')
+
+
+def print_helper():
+    for stud in student_list_helper:
+        print_stud(stud)
 
 
 def sort_by_avg():
-    pass
+    #global students_list
+    students_stud = sorted(students_list, key=lambda k: k['srednia_ocen'])
+    for stud in students_stud:
+        print_stud_short_with_key(stud, 'srednia_ocen')
+    #students_list = students_stud
 
 
 def sort_by_surname():
-    pass
+    students_stud = sorted(students_list, key=lambda k: k['nazwisko'])
+    for stud in students_stud:
+        print_stud_short_with_key(stud, 'nazwisko')
 
 
 def find_by_pesel():
-    pass
+    pesel = input('Podaj pesel:')
+    filtered_list = filter(lambda k: k['PESEL'] == pesel, students_list)
+    for stud in filtered_list:
+        print_all(stud)
 
 
 def remove_by_pesel():
-    pass
+    global students_list
+    pesel = input('Podaj pesel:')
+    students_list = filter(lambda k: k['PESEL'] != pesel, students_list)
 
 
-def copy_deep():
-    pass
-
-
-def copy():
-    pass
+def copy_by_avg():
+    student_list_helper.clear()
+    max_avg = float(input('Max. średnia: '))
+    filtered_list = filter(lambda k: k['srednia_ocen'] < max_avg, students_list)
+    for stud in filtered_list:
+        student_list_helper.append(stud)
+    print_helper()
 
 
 def exit_menu():
     quit()
 
 
-menu: Dict[int, Union[Callable[[],None], Callable[[],None], Callable[[],None], Callable[[],None]]] = {
+menu: Dict[int, Union[Callable[[], None], Callable[[], None], Callable[[], None], Callable[[], None]]] = {
     0: add_student,
     1: print_all,
     2: sort_by_avg,
     3: sort_by_surname,
     4: find_by_pesel,
     5: remove_by_pesel,
-    6: copy_deep,
-    7: copy,
+    6: copy_by_avg,
+    7: print_helper,
     8: exit_menu,
 }
 
 
 def run_app():
     while True:
-        for key in sorted(menu):
+        for key in menu:
             print(key, '=>', menu[key].__name__)
         option = input('select from menu:')
         if not option.isnumeric():
             print('You have to pick a number.')
         elif int(option) in menu:
-            print('done')
+            menu.get(int(option))()
         else:
             print('Option not found. Please, pick again.')
 
 
 if __name__ == '__main__':
     run_app()
-
