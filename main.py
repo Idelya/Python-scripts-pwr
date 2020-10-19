af8bd20d132fb5be540d5009eb682e3be6fb27c7
Anna <annapapros21@gmail.com>
2020-10-19 16:54:44 +0200

    text menu and list off students

--- main.py
+++ main.py
@@ -1,16 +1,69 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from typing import List, Dict, Callable, Union
+from students import students_arr
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def add_student():
+    pass
 
 
-# Press the green button in the gutter to run the script.
+def print_all():
+    pass
+
+
+def sort_by_avg():
+    pass
+
+
+def sort_by_surname():
+    pass
+
+
+def find_by_pesel():
+    pass
+
+
+def remove_by_pesel():
+    pass
+
+
+def copy_deep():
+    pass
+
+
+def copy():
+    pass
+
+
+def exit_menu():
+    quit()
+
+
+menu: Dict[int, Union[Callable[[],None], Callable[[],None], Callable[[],None], Callable[[],None]]] = {
+    0: add_student,
+    1: print_all,
+    2: sort_by_avg,
+    3: sort_by_surname,
+    4: find_by_pesel,
+    5: remove_by_pesel,
+    6: copy_deep,
+    7: copy,
+    8: exit_menu,
+}
+
+
+def run_app():
+    while True:
+        for key in sorted(menu):
+            print(key, '=>', menu[key].__name__)
+        option = input('select from menu:')
+        if not option.isnumeric():
+            print('You have to pick a number.')
+        elif int(option) in menu:
+            print('done')
+        else:
+            print('Option not found. Please, pick again.')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    run_app()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ students.py
@@ -0,0 +1,88 @@
+from typing import List, Dict, Union
+
+students_arr: List[Dict[str, Union[str, str, str, str, int, float, str]]] = [{
+    'imie': 'Agata',
+    'nazwisko': 'Walerczyk',
+    'PESEL': '98031359375',
+    'plec': 'K',
+    'rok_studiow': 4,
+    'srednia_ocen': 4.23,
+    'kierunek_studiow': 'Zarzadzanie'
+},
+{
+    'imie': 'Michal',
+    'nazwisko': 'Koszyk',
+    'PESEL': '99090365372',
+    'plec': 'M',
+    'rok_studiow': 2,
+    'srednia_ocen': 2.23,
+    'kierunek_studiow': 'Zarzadzanie'
+},
+{
+    'imie': 'Lucjan',
+    'nazwisko': 'Kowalczyk',
+    'PESEL': '991018590374',
+    'plec': 'M',
+    'rok_studiow': 3,
+    'srednia_ocen': 3.75,
+    'kierunek_studiow': 'Informatyka'
+},
+{
+    'imie': 'Jakub',
+    'nazwisko': 'Treczyk',
+    'PESEL': '00100649938',
+    'plec': 'M',
+    'rok_studiow': 1,
+    'srednia_ocen': 5.03,
+    'kierunek_studiow': 'Informatyka'
+},
+{
+    'imie': 'Ewelina',
+    'nazwisko': 'Malarcz',
+    'PESEL': '99021679675',
+    'plec': 'K',
+    'rok_studiow': 3,
+    'srednia_ocen': 4.53,
+    'kierunek_studiow': 'Zarzadzanie'
+},{
+    'imie': 'Joanna',
+    'nazwisko': 'Kruk',
+    'PESEL': '99022674947',
+    'plec': 'K',
+    'rok_studiow': 4,
+    'srednia_ocen': 4.23,
+    'kierunek_studiow': 'Informatyka'
+},{
+    'imie': 'Agata',
+    'nazwisko': 'Roczek',
+    'PESEL': '96050458429',
+    'plec': 'K',
+    'rok_studiow': 4,
+    'srednia_ocen': 3.34,
+    'kierunek_studiow': 'Zarzadzanie'
+},
+{
+    'imie': 'Hubert',
+    'nazwisko': 'Gil',
+    'PESEL': '98031954332',
+    'plec': 'M',
+    'rok_studiow': 2,
+    'srednia_ocen': 4.83,
+    'kierunek_studiow': 'Informatyka'
+},{
+    'imie': 'Filip',
+    'nazwisko': 'Dryń',
+    'PESEL': '00031359374',
+    'plec': 'M',
+    'rok_studiow': 1,
+    'srednia_ocen': 4.5,
+    'kierunek_studiow': 'Informatyka'
+},{
+    'imie': 'Paulina',
+    'nazwisko': 'Kwaitkowska',
+    'PESEL': '99111132145',
+    'plec': 'K',
+    'rok_studiow': 4,
+    'srednia_ocen': 4.93,
+    'kierunek_studiow': 'Informatyka'
+}]
